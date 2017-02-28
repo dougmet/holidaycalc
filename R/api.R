02ba0b7e084dbad312fdc59a2a3092bd9f0418fa
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-02-28 16:26:02 +0000

    make it a date related package

--- R/api.R
+++ R/api.R
@@ -1,27 +1,11 @@
 
 # Taken from trestletech/plumber
 
-#' Mean of Normals
-#'
-#' Given a number of samples calculate the mean of
-#' the standard normals
-#'
-#' @param samples integer
+#' Return today's Date
 #'
 #' @export
-#* @get /mean
-normalMean <- function(samples=10){
-  data <- stats::rnorm(samples)
-  mean(data)
+#* @get /today
+todayDate <- function(){
+  lubridate::today()
 }
 
-#' Add Two
-#'
-#' @param a number 1
-#' @param b number 2
-#' @return a + b
-#' @export
-#* @post /sum
-addTwo <- function(a, b){
-  as.numeric(a) + as.numeric(b)
-}
