bfc2a7c4f43844f3b9c8b86a32bc3473906068b4
Douglas Ashton <douglas.j.ashton@gmail.com>
2017-03-05 20:13:21 +0000

    add the minDaysLeave function as endpoint

--- R/api.R
+++ R/api.R
@@ -15,7 +15,15 @@ nowTime <- function(){
 # Call the longestRun function
 #
 #* @get /longrun
-longrun <- function(start, end, daysleave) {
+#* @post /longrun
+longrun <- function(start = Sys.Date(), end = Sys.Date() + 370, daysleave = 10) {
   daysleave <- as.integer(daysleave)
   holidaycalc::longestRun(startDate = start, endDate = end, daysLeave = daysleave)
+}
+
+#* @get /minleave
+#* @post /minleave
+minleave <- function(start = Sys.Date(), end = Sys.Date() + 370, window = 21) {
+  window <- as.integer(window)
+  holidaycalc::minDaysLeave(startDate = start, endDate = end, window = window)
 }
